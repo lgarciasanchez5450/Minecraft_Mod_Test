ef06ee926144ae0f2c252dd0ff5dfc53221f3e34
lgarciasanchez5450 <lgarciasanchez5450@gmail.com>
2023-07-10 20:47:49 -0700

    Secondary Commit

--- src/main/java/com/example/examplemod/item/ModItems.java
+++ src/main/java/com/example/examplemod/item/ModItems.java
@@ -14,7 +14,7 @@ public class ModItems {
 
     public static final RegistryObject <Item> _ITEM_NAME_ = ITEMS.register("_item_identifier_",() -> new Item(new Item.Properties()));
 
-    private static FoodProperties thing  = new FoodProperties.Builder().alwaysEat().nutrition(100).saturationMod(20).fast().build();
+    private static final FoodProperties thing  = new FoodProperties.Builder().alwaysEat().nutrition(100).saturationMod(20).fast().build();
     public static final RegistryObject <Item> PERFECT_FOOD = ITEMS.register("perfect_food",() -> new Item(new Item.Properties().food(thing).fireResistant().stacksTo(3)));
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
