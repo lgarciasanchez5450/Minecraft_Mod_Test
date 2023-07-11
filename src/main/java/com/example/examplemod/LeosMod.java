da9a3219fe7cec397cb2de3bada47285e3bf6d0a
lgarciasanchez5450 <lgarciasanchez5450@gmail.com>
2023-07-10 20:44:40 -0700

    Initial Commit

---
+++ src/main/java/com/example/examplemod/LeosMod.java
@@ -0,0 +1,73 @@
+package com.example.examplemod;
+import com.example.examplemod.item.ModItems;
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(LeosMod.MOD_ID)
+public class LeosMod
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MOD_ID = "leos_mod";//lowercase only, underscore and no space, numbers allowed
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+
+    public LeosMod()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+
+        ModItems.register(modEventBus);
+
+        modEventBus.addListener(this::addCreative);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    // Add the example block item to the building blocks tab
+    private void addCreative(BuildCreativeModeTabContentsEvent event)
+    {
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems._ITEM_NAME_);
+
+        } else if (event.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS) {
+            event.accept(ModItems.PERFECT_FOOD);
+        }
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+
+        }
+    }
+}
---
+++ src/main/java/com/example/examplemod/item/ModItems.java
@@ -0,0 +1,22 @@
+package com.example.examplemod.item;
+
+import com.example.examplemod.LeosMod;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, LeosMod.MOD_ID);
+
+    public static final RegistryObject <Item> _ITEM_NAME_ = ITEMS.register("_item_identifier_",() -> new Item(new Item.Properties()));
+
+    private static FoodProperties thing  = new FoodProperties.Builder().alwaysEat().nutrition(100).saturationMod(20).fast().build();
+    public static final RegistryObject <Item> PERFECT_FOOD = ITEMS.register("perfect_food",() -> new Item(new Item.Properties().food(thing).fireResistant().stacksTo(3)));
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
